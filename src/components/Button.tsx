153fe3777e8a3370cb2fa316c0b2c97f9afff083
Igor Quiterio <idcq101@gmail.com>
2024-10-08 23:02:41 -0300

    header

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles"
-
-interface ButtonProps {
-  variant: ButtonVariant
-}
-
-export function Button(){
-  return <ButtonContainer>Enviar</ButtonContainer>
-}
